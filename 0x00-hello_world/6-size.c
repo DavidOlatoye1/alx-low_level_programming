547f80d652af2809526c0d7e688f0e31072d0def
DavidOlatoye1 <davidolatoye19@gmail.com>
2022-07-03 11:47:03 -0700

    1

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,7 +5,7 @@
  * main - Entry point
  *   Return: Always 0 (Success)
 */
-int main(void)
+int main(0)
 {
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
