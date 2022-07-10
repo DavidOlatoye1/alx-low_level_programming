784e60bc7f458874a7901eae031a7dc7dd8d4bfc
DavidOlatoye1 <davidolatoye19@gmail.com>
2022-07-10 10:36:49 -0700

    1234

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,6 @@
 #include "main.h"
 
 
-
 /**
  * print_alphabet - Prints the alphabet in lowercase.
  * Return: Always 0 (Success)
@@ -11,8 +10,8 @@ void print_alphabet(void)
 
 {
 	int i;
-	
-	for (i = 97; i <= 122; i++)
+
+	for (i = 97; i <= 122; 1++)
 	{
 		_putchar(i);
 	}
