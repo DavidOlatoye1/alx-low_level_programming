809939a6cd9a1ea05b7c7fb696972b765d4600a9
DavidOlatoye1 <davidolatoye19@gmail.com>
2022-07-10 11:11:03 -0700

    ff

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -14,11 +14,9 @@ void print_alphabet_x10(void)
 	for (a = 0; a <= 9; a++)
 	{
 		for (b = 97; b <= 122; b++)
+		{
 			_putchar(b);
+		}
+		_putchar('\n');
 	}
-
-									_putchar('\n');
-
-										}
-
 }
