a875066f8c4a0456b46924ccd920b3fe4386afbc
DavidOlatoye1 <davidolatoye19@gmail.com>
2022-07-12 16:28:06 -0700

    hgf

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,6 +2,7 @@
 
 
 
+
 /**
  *  _strlen - returns the length of a string.
  *   @s: input string.
@@ -11,7 +12,7 @@
 int _strlen(char *s)
 {
 	int count = 0;
-	
+
 	while (*(s + count) != '\0')
 		count++;
 	return (count);
