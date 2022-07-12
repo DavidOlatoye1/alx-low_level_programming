7c9ce8a55c4acb2bbb0c462a586184de17715069
DavidOlatoye1 <davidolatoye19@gmail.com>
2022-07-12 16:38:03 -0700

    hgf

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,15 +3,15 @@
 
 
 /**
- *  _puts - prints a string, followed by a new line.
- *   @str: input string.
- *   Return: no return.
+ *   _puts - prints a string, followed by a new line.
+ *    @str: input string.
+ *    Return: no return.
  */
 
 void _puts(char *str)
 {
 	int count = 0;
-	while (count <= 0)
+	while (count >= 0)
 	{
 		if (str[count] == '\0')
 		{
