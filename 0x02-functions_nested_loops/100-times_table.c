f472d5af39b5c2a2fc4bf71eaf81caaa83f38c0a
DavidOlatoye1 <davidolatoye19@gmail.com>
2022-07-27 04:02:58 -0700

    win

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,6 +11,7 @@
 void print_times_table(int n)
 {
 	int a, b, op;
+	
 	if (n >= 0 && n <= 15)
 	{
 		for (a = 0; a <= n; a++)
@@ -27,20 +28,20 @@ void print_times_table(int n)
 					_putchar(32);
 					_putchar(op + 48);
 				}
-						else if (op <= 99)
-						{
-							_putchar(32);
-							_putchar((op / 10) + 48);
-							_putchar((op % 10) + 48);
-						}
-								else
-								{
-									_putchar(((op / 100) % 10) + 48);
-									_putchar(((op / 10) % 10) + 48);
-									_putchar((op % 10) + 48);
-								}
+				else if (op <= 99)
+				{
+					_putchar(32);
+					_putchar((op / 10) + 48);
+					_putchar((op % 10) + 48);
+				}
+				else
+				{
+					_putchar(((op / 100) % 10) + 48);
+					_putchar(((op / 10) % 10) + 48);
+					_putchar((op % 10) + 48);
+				}
 			}
-							_putchar('\n');
+			_putchar('\n');
 		}
 	}
 }
