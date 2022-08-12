98828e84f7f7f6a2f35bcab5cd783275e9e6e045
DavidOlatoye1 <davidolatoye19@gmail.com>
2022-08-11 19:06:58 -0700

    f

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -19,8 +19,9 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	new->str = strdup(str);
 	for (nchar = 0; str[nchar]; nchar++)
-		new->len = nchar
-			new->next = *head;
+		;
+	new->len = nchar
+		new->next = *head;
 	*head = new;
 	return (*head);
 }
